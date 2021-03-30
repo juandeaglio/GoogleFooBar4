99c2dcefbd2cca28e276db085f9da78955bb63ba
juandeaglio <45726108+juandeaglio@users.noreply.github.com>
2021-03-30 15:14:50 -0700

    Working on additional cases

--- src/FuelController.java
+++ src/FuelController.java
@@ -4,93 +4,79 @@ import java.util.HashMap;
 public class FuelController
 {
     //BigInteger MAX_INT_VALUE = (2 ^ 32) ^ Integer.MAX_VALUE;
-    HashMap<String, Integer> dict = new HashMap<>();
+
     static StringBuilder quotient = new StringBuilder();
     static BigInteger quotientBigInt;
     public int Optimize(String pelletStart)
     {
-
         int steps = 0;
-        if(pelletStart.length() == 1 && Integer.parseInt(pelletStart) == 1)
-            return steps;
-        else if(dict.containsKey(pelletStart))
+        String binaryBig = new BigInteger(pelletStart).toString(2);
+        if(binaryBig.length()>=3)
         {
-            return dict.get(pelletStart);
+            binaryBig = binaryBig.substring(binaryBig.length()-3);
         }
-        else
+
+        if(new BigInteger(pelletStart).compareTo(BigInteger.ONE) == 0 && Integer.parseInt(pelletStart) == 1)
+            return steps;
+
+        else if(binaryBig.charAt(binaryBig.length()-1) == '0')
         {
-            String lastDigit = pelletStart.substring(pelletStart.length()-1);
-            if(Integer.parseInt(lastDigit) % 2 == 0)
+            int afterSteps = 0;
+            String lastDigit = pelletStart.substring(pelletStart.length() - 1);
+            if (Integer.parseInt(lastDigit) % 2 == 0)
             {
-                String temp = pelletStart;
-                while(Integer.parseInt(lastDigit) % 2 == 0)
+                while (Integer.parseInt(lastDigit) % 2 == 0)
                 {
                     quotientBigInt = new BigInteger(pelletStart);
                     LongDivisionBy2(pelletStart);
                     pelletStart = quotient.toString();
-                    if(pelletStart.length() == 1)
+                    if (pelletStart.length() == 1)
                         lastDigit = pelletStart;
                     else
-                        lastDigit = pelletStart.substring(pelletStart.length()-1);
+                        lastDigit = pelletStart.substring(pelletStart.length() - 1);
                     steps++;
                 }
-                int afterSteps = Optimize(pelletStart);
-                if(afterSteps > 2)
-                    dict.put(temp, steps + afterSteps);
-                return steps + afterSteps;
+                afterSteps = Optimize(pelletStart);
+
             }
-            else
+            return steps + afterSteps;
+        }
+        else
+        {
+            int totalSteps;
+            String optimizationArg;
+            String numberWithoutLastDigit;
+            if(Integer.parseInt(binaryBig,2) == 7)
             {
-                String leftOptimizationArg;
-                String rightOptimizationArg;
-                int leftSteps = Integer.MAX_VALUE;
-                int rightSteps = Integer.MAX_VALUE;
-                String numberWithoutLastDigit;
-                if(pelletStart.length() > 1)
+                String lastDigit = pelletStart.substring(pelletStart.length() - 1);
+                if (pelletStart.length() > 1)
                 {
                     numberWithoutLastDigit = pelletStart.substring(0, pelletStart.length() - 1);
-
                 }
                 else
                 {
                     numberWithoutLastDigit = "";
                 }
-                leftOptimizationArg = numberWithoutLastDigit + (Integer.parseInt(lastDigit) - 1);
-                rightOptimizationArg = numberWithoutLastDigit + (Integer.parseInt(lastDigit) + 1);
-
-                if(amountOf2s(leftOptimizationArg) > amountOf2s(rightOptimizationArg))
-                {
-                    leftSteps = Optimize(leftOptimizationArg);
+                optimizationArg = numberWithoutLastDigit + (Integer.parseInt(lastDigit) + 1);
+                totalSteps = Optimize(optimizationArg);
+            }
+            else
+            {
+                String lastDigit = pelletStart.substring(pelletStart.length() - 1);
+                if (pelletStart.length() > 1) {
+                    numberWithoutLastDigit = pelletStart.substring(0, pelletStart.length() - 1);
                 }
                 else
-                    rightSteps = Optimize(rightOptimizationArg);
-                int totalSteps = Math.min(leftSteps, rightSteps);
-                if(totalSteps+1 > 2)
                 {
-                    if(leftSteps == totalSteps)
-                    {
-                        dict.put(leftOptimizationArg, totalSteps);
-                    }
-                    else
-                    {
-                        dict.put(rightOptimizationArg, totalSteps);
-                    }
+                    numberWithoutLastDigit = "";
                 }
-                return 1 + totalSteps;
+                optimizationArg = numberWithoutLastDigit + (Integer.parseInt(lastDigit) - 1);
+                totalSteps = Optimize(optimizationArg);
             }
+            return 1 + totalSteps;
         }
     }
-    public int amountOf2s(String number)
-    {
-        int timesDivided = 0;
-        quotientBigInt = new BigInteger(number);
-        while(quotientBigInt.getLowestSetBit() != 0)
-        {
-            quotientBigInt = quotientBigInt.divide(BigInteger.valueOf(2));
-            timesDivided++;
-        }
-        return timesDivided;
-    }
+
     public void LongDivisionBy2(String number)
     {
         quotient.setLength(0);
